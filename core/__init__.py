e2a4fee47135537407137b61bfbb799db3d3d725
Guglielmo Cimolai <guglielmo.cimolai@icloud.com>
2025-07-10 15:45:20 +0200

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,1 +1,15 @@
-"""Core components for TSP GA."""
+"""Core functionality for TSP solver."""
+from .cities import get_fixed_cities, generate_cities
+from .distance import euclidean_distance, calculate_distance_matrix, calculate_tour_cost
+from .individual import Individual
+from .dynamic_cities import DynamicCityManager
+
+__all__ = [
+    'get_fixed_cities',
+    'generate_cities',
+    'euclidean_distance',
+    'calculate_distance_matrix',
+    'calculate_tour_cost',
+    'Individual',
+    'DynamicCityManager'
+]
